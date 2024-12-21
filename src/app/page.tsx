c5691e7905febcbc99852bc05095e9fa2d0a3761
anton <antonosland789@gmail.com>
2024-12-20 23:22:14 -0600

    inital keyboard rendering properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,58 @@
+"use client";
+import { MouseEventHandler, useState } from "react";
+
 import Nav from "@/components/Nav";
 import Piano from "@/components/piano/Piano";
 
 export default function Home() {
+  const keys = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+
+  const keyButtonPress = (event: Event) => {
+    const currentButton = event.target;
+    if (currentButton) {
+      console.log(currentButton);
+    }
+  };
+
   return (
     <main className="flex flex-col h-screen w-screen">
       <Nav />
       <section className="h-[91%] flex flex-col justify-center items-center">
-        <div>
-          <Piano />
+        <div className="flex border-t border-black scale-[80%]">
+          {[1, 2, 3, 4].map((_, i) => (
+            <Piano key={"piano-component-" + i + 1} keyAttr={i} keys={keys} />
+          ))}
+        </div>
+        <div className="flex items-center w-[75%]">
+          <div className="w-[33%] flex flex-col justify-center items-center">
+            <p className="text-center">
+              Pick a key to start building a chord:{" "}
+            </p>
+            <div className="w-[75%] flex flex-wrap justify-center ">
+              {keys.map((key, i) => (
+                <button
+                  key={i}
+                  className={`border-2 border-black rounded-lg px-2 m-1 w-fit`}
+                  // onClick={keyButtonPress}
+                >
+                  {key}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     </main>
--- src/components/piano/Piano.tsx
+++ src/components/piano/Piano.tsx
@@ -1,18 +1,13 @@
-export default function Piano() {
-  const keys = [
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-  ];
+import { Fragment } from "react";
+
+export default function Piano({
+  keyAttr,
+  keys,
+}: {
+  keyAttr: number;
+  keys: string[];
+}) {
+  const blackKeys = ["C#", "D#", "F#", "G#", "A#"];
 
   const fullPiano = [];
 
@@ -23,19 +18,38 @@ export default function Piano() {
   }
 
   return (
-    <div className="w-[1000px] h-[100px] flex border-2 border-black relative">
-      {fullPiano.map((key, i) => (
-        <>
-          {key[1] === "#" ? (
+    <div
+      key={"piano-container-" + keyAttr}
+      className="w-[350px] h-[140px] flex relative space-x-[0.5px] mx-[0.25px]"
+    >
+      {keys.map(
+        (key, i) =>
+          key[1] !== "#" && (
             <div
-              className={`w-[10px] h-[60%] bg-black absolute`}
-              style={{left: ``}}
-            ></div>
-          ) : (
-            <div className="h-full w-[40px] border border-black">{key}</div>
-          )}
-        </>
-      ))}
+              key={"white-keys-" + i + keyAttr}
+              className="w-[50px] flex justify-center items-end border border-black rounded-b-lg shadow-lg"
+            >
+              {/* {key} */}
+            </div>
+          )
+      )}
+      <div className="absolute flex h-[65%] w-[350px] space-x-[0.5px]">
+        {blackKeys.map((key, i) => (
+          <div
+            key={"black-keys-" + i + keyAttr}
+            className="relative flex justify-center items-end h-full w-[7%] bg-black rounded-b-lg shadow-lg text-white"
+            style={{
+              left: `${
+                i === 2 || i === 3 || i === 4
+                  ? 8 * (i + 1) - i + 17.3
+                  : 8 * (i + 1) - i + 2.75
+              }%`,
+            }}
+          >
+            {/* {key} */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
